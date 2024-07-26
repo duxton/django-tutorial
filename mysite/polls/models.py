6ca9ab8d0d432e0578b3e417928d2eb96e83720f
duxton <duxtonlim2000@gmail.com>
2024-07-26 16:55:27 +0800

    Django framework tutorial

---
+++ mysite/polls/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib import admin
+# Create your models here.
+class generatedDescription(models.Model):
+    username = models.CharField(max_length=255)
+    image_path = models.CharField(max_length=255)
+    image_data = models.CharField(max_length=255)
+    insert_datetime = models.DateTimeField(default=datetime.datetime.now)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
